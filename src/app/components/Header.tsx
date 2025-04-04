ff04cee80428baeb2c7ff8f2801aafa617196102
mattfisher <nuggsrocks@yahoo.com>
2025-04-04 19:10:46 -0400

    improve responsive menu

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,47 +1,65 @@
 'use client'
 
-import React, { useRef } from 'react';
-import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import styles from './Header.module.scss';
-import Link from 'next/link';
-import { routes } from '../../../constants';
+import React, { useRef } from 'react'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import styles from './Header.module.scss'
+import Link from 'next/link'
+import { routes } from '../../../constants'
+import { usePathname } from 'next/navigation'
 
 export default function Header() {
-    const ref = useRef(null);
+    const pn = usePathname()
+    const ref = useRef(null)
 
-    const toggle = () => {
+    const toggle = (instant = false) => {
         if (ref.current) {
-            if (ref.current.style.height === "0px" || !ref.current.style.height) {
-                ref.current.style.height = ref.current.scrollHeight + "px";
+            if (instant) {
+                ref.current.classList.add(styles.instant)
             } else {
-                ref.current.style.height = "0px";
+                ref.current.classList.remove(styles.instant)
+            }
+
+            if (ref.current.style.height === '0px' || !ref.current.style.height) {
+                ref.current.style.height = ref.current.scrollHeight + 'px'
+            } else {
+                ref.current.style.height = '0px'
             }
         }
-    };
+    }
 
     return (
-        <header>
-            <div className={styles.header}>
-                <Image
-                    className={styles.logo}
-                    src='/customcolor_icon_transparent_background.png'
-                    alt='Crackerjack Solutions logo'
-                    width={180}
-                    height={180}
-                    priority
-                />
-                <button onClick={toggle} className={styles.toggle}>
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <Link href='/'>
+                    <Image
+                        className={styles.logo}
+                        src='/customcolor_icon_transparent_background.png'
+                        alt='Crackerjack Solutions logo'
+                        width={180}
+                        height={180}
+                        priority
+                    />
+                </Link>
+
+                <nav className={styles.desktopNav}>
+                    {routes.map((l, key) => {
+                        return <Link className={`${styles.link} ${pn === l[0] ? styles.active : ''}`} key={key} href={l[0]}>{l[1]}</Link>
+                    })}
+                </nav>
+
+
+                <button onClick={() => toggle()} className={styles.toggle}>
                     <FontAwesomeIcon icon={faBars} />
                 </button>
             </div>
 
-            <nav ref={ref} className={styles.nav}>
+            <nav ref={ref} className={styles.mobileNav}>
                 {routes.map((l, key) => {
-                    return <Link className={styles.link} key={key} href={l[0]}>{l[1]}</Link>
+                    return <Link className={`${styles.link} ${pn === l[0] ? styles.active : ''}`} key={key} href={l[0]} onClick={() => toggle(true)}>{l[1]}</Link>
                 })}
             </nav>
         </header>
-    );
+    )
 }
