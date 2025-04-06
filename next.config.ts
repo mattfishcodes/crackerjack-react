c6ad0fcb4e2ff92f1505012189036a267856d692
mattfisher <nuggsrocks@yahoo.com>
2025-04-05 22:58:20 -0400

    use scss variables file

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  
-  sassOptions: {
-    additionalData: `
-      $primary: #3a0756;
-      $secondary: #e6b01d;
-      $light: #f9fafb;
-    `
-  }
 };
 
 export default nextConfig;
