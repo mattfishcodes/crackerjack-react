681a92d1dca8eface5a4632f7854ca2d29e8f321
mattfisher <nuggsrocks@yahoo.com>
2025-04-07 23:57:56 -0400

    fix vscode eslint integration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,30 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
-      "quotes": [
-        "error", 
-        "single",
+      'quotes': [
+        'error', 
+        'single',
         {
-          "avoidEscape": true,
-          "allowTemplateLiterals": true
+          'avoidEscape': true,
+          'allowTemplateLiterals': true
         }
       ],
-      "jsx-quotes": ["error", "prefer-single"],
-      "semi": ["error", "never"]
+      'jsx-quotes': ['error', 'prefer-single'],
+      'semi': ['error', 'never']
     }
   }
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-};
+}
 
-export default nextConfig;
+export default nextConfig
