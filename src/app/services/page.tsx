286173dcd91fb7d1968361d05b56d9415edcfb98
mattfisher <nuggsrocks@yahoo.com>
2025-04-09 19:56:53 -0400

    add more content

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,19 +2,24 @@ import React from 'react'
 import styles from './page.module.scss'
 import Section from '../components/Section'
 import ScheduleButton from '../components/ScheduleButton'
+import Header from '../components/Header'
 
 export default function Services() {
   return (
     <main>
       <Section>
         <div style={{ textAlign: 'center' }}>
-          <h3>Interested in Exploring How our Services Can Benefit You?</h3>
+          <h3 style={{ lineHeight: '2.25rem', marginBottom: '20px' }}>
+            Interested in Exploring How our Services Can Benefit You?
+          </h3>
 
           <p>Schedule your complimentary 30 minute call below.</p>
+
           <p>
             Let&apos;s explore how to navigate life&apos;s unpredictability
             together and find that equilibrium you&apos;re searching for.
           </p>
+
           <ScheduleButton color='light' />
         </div>
       </Section>
@@ -24,63 +29,59 @@ export default function Services() {
       <Section>
         <div className={styles.flexbox}>
           <div className={styles.flexitem}>
-            <h3 className='secondary'>Systems Analysis</h3>
+            <h3>Systems Analysis</h3>
 
             <p>
               Identify bottlenecks, inefficiencies, and areas for improvement
               within your organization&apos;s operations.
             </p>
 
-            <hr className='secondary' />
+            <hr />
 
             <p>
-              <strong className='secondary'>
-                Understanding Your Processes
-              </strong>
+              <strong>Understanding Your Processes</strong>
               <br />
               We&apos;ll meet with you to understand your current processes,
               systems, and workflows.
             </p>
 
             <p>
-              <strong className='secondary'>Thorough Evaluation</strong>
+              <strong>Thorough Evaluation</strong>
               <br />
               Our team will conduct a detailed evaluation of your current state.
             </p>
 
             <p>
-              <strong className='secondary'>Actionable Recommendations</strong>
+              <strong>Actionable Recommendations</strong>
               <br />
               You&apos;ll receive a customized report with actionable
               recommendations.
             </p>
           </div>
-          <div className={`${styles.flexitem} light bg-primary`}>
-            <h3 className='secondary'>CRM Setup</h3>
+          <div className={styles.flexitem}>
+            <h3>CRM Setup</h3>
 
             <p>
               Streamline your client relationships with a tailored customer
               relationship management solution.
             </p>
 
-            <hr className='secondary' />
+            <hr />
 
             <p>
-              <strong className='secondary'>Centralized Platform</strong>
+              <strong>Centralized Platform</strong>
               <br />
               Manage all client and potential client relationships seamlessly.
             </p>
 
             <p>
-              <strong className='secondary'>
-                Stronger Business Relationships
-              </strong>
+              <strong>Stronger Business Relationships</strong>
               <br />
               Leverage our expertise to cultivate strong connections.
             </p>
 
             <p>
-              <strong className='secondary'>Improved Communication</strong>
+              <strong>Improved Communication</strong>
               <br />
               Streamline processes and watch your bottom line soar.
             </p>
@@ -91,7 +92,9 @@ export default function Services() {
       <Section>
         <div>
           <h3>Hourly Retainer Packages</h3>
-          <p style={{ fontFamily: 'var(--font-montserrat)' }}>
+          <p
+            style={{ fontFamily: 'var(--font-montserrat)', fontWeight: '500' }}
+          >
             Flexible Solutions for Your Business Needs
           </p>
           <p>
@@ -162,40 +165,134 @@ export default function Services() {
 
       <Section>
         <div>
-          Ready to Get Started? Invest in your business&apos;s success with a
-          flexible hourly retainer package from Crackerjack Solutions. Contact
-          us today to schedule your free consultation and discover how we can
-          help you achieve your goals. Schedule Now Get access to a FREE 30
-          minute call!
+          <h2>Ready to Get Started?</h2>
+          <p>
+            Invest in your business&apos;s success with a flexible hourly
+            retainer package from Crackerjack Solutions. Contact us today to
+            schedule your free consultation and discover how we can help you
+            achieve your goals.
+          </p>
+          <ScheduleButton color='light' />
+        </div>
+      </Section>
+
+      <Section>
+        <div className={styles.flexContainer}>
+          <div className={styles.flexItem}>
+            <h3>Projects</h3>
+            <hr />
+            <ul>
+              <li>
+                <span className={styles.bullet}></span>
+                <span>Create Detailed Project Plans</span>
+              </li>
+              <li>Lead Initial Kickoff Call with Stakeholders</li>
+              <li>Lead Regular Check In Calls</li>
+              <li>
+                Manage Communication, Timeline, Change Requests, Status and
+                Deliverables
+              </li>
+              <li>
+                Maximize Productivity, Quality and Stakeholder Satisfaction
+              </li>
+              <li>And more…</li>
+            </ul>
+          </div>
+          <div className={styles.flexItem}>
+            <h3>Systems</h3>
+            <hr />
+            <ul>
+              <li>Manage Scheduling</li>
+              <li>Manage Invoicing</li>
+              <li>Manage Client Onboarding</li>
+              <li>Review, Update and Create Processes</li>
+              <li>Create SOPs & Playbooks for Systems</li>
+              <li>And more…</li>
+            </ul>
+          </div>
+          <div className={styles.flexItem}>
+            <h3>Operations</h3>
+            <hr />
+            <ul>
+              <li>Review and Optimize existing Business Processes and Tools</li>
+              <li>Create SOPs and Playbooks for Operations</li>
+              <li>Manage CRM, Email and Calendar</li>
+              <li>Create and/or Update Automations</li>
+              <li>Develop Digital File Systems</li>
+              <li>And more…</li>
+            </ul>
+          </div>
         </div>
       </Section>
 
       <Section>
         <div>
-          Services Available Projects Create Detailed Project Plans Lead Initial
-          Kickoff Call with Stakeholders Lead Regular Check In Calls Manage
-          Communication, Timeline, Change Requests, Status and Deliverables
-          Maximize Productivity, Quality and Stakeholder Satisfaction And more…
-          Systems Manage Scheduling Manage Invoicing Manage Client Onboarding
-          Review, Update and Create Processes Create SOPs & Playbooks for
-          Systems And more… Operations Review and Optimize existing Business
-          Processes and Tools Create SOPs and Playbooks for Operations Manage
-          CRM, Email and Calendar Create and/or Update Automations Develop
-          Digital File Systems And more…
-        </div>
-      </Section>
-
-      <Section>
-        <div>
-          Software We use a variety of online apps/SaaS tools. Some of them are
-          used daily and some weekly, others less often. However, we have not
-          yet met a SaaS tool that we were not able to master! We learn quickly
-          and love the challenge of learning a new tool. Here is a brief list of
-          some of the tools we currently use or have used frequently: Airtable
-          Asana Calendly Canva ClickUp Dubsado FormAssembly Gusto HubSpot
-          LastPass Loom MailChimp Make Microsoft/Office 365 Miro Moxie
-          QuickBooks Salesforce SiteGround Slack Stripe Payments TaskRay Trello
-          WordPress Zapier Zoom
+          <p>
+            Software We use a variety of online apps/SaaS tools. Some of them
+            are used daily and some weekly, others less often. However, we have
+            not yet met a SaaS tool that we were not able to master! We learn
+            quickly and love the challenge of learning a new tool. Here is a
+            brief list of some of the tools we currently use or have used
+            frequently:
+          </p>
+          <div>
+            <div>
+              Airtable
+              <br />
+              Asana
+              <br />
+              Calendly
+              <br />
+              Canva
+              <br />
+              ClickUp
+              <br />
+              Dubsado
+              <br />
+              FormAssembly
+              <br />
+              Gusto
+              <br />
+              HubSpot
+              <br />
+              LastPass
+              <br />
+              Loom
+              <br />
+              MailChimp
+              <br />
+              Make
+            </div>
+            <div>
+              Microsoft/Office 365
+              <br />
+              Miro
+              <br />
+              Moxie
+              <br />
+              QuickBooks
+              <br />
+              Salesforce
+              <br />
+              SiteGround
+              <br />
+              Slack
+              <br />
+              Stripe
+              <br />
+              Payments
+              <br />
+              TaskRay
+              <br />
+              Trello
+              <br />
+              WordPress
+              <br />
+              Zapier
+              <br />
+              Zoom
+            </div>
+          </div>
         </div>
       </Section>
     </main>
