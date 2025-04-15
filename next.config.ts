d94616720b58b892e7f0aa66dd2df97443ea5062
mattfisher <nuggsrocks@yahoo.com>
2025-04-15 19:18:27 -0400

    add global scss to next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  sassOptions: {
+    additionalData: `
+      @use "@/app/mixins" as *;
+      @use "@/app/variables" as *;
+    `,
+  },
 }
 
 export default nextConfig
