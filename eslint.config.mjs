b7336371f7bd1b2b93f7ff4abe90151770454253
mattfisher <nuggsrocks@yahoo.com>
2025-04-20 21:45:24 -0400

    rehash formatting setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
-import prettier from 'eslint-config-prettier'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -13,15 +12,8 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends(
     'next/core-web-vitals',
-    'next/typescript',
-    'plugin:prettier/recommended',
+    'next/typescript'
   ),
-  prettier,
-  {
-    rules: {
-      indent: 'off',
-    },
-  },
 ]
 
 export default eslintConfig
