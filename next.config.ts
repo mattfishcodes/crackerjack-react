2488f3bc78c9b94489868e255dd5cffce5df3992
mattfisher <nuggsrocks@yahoo.com>
2025-04-22 18:54:26 -0400

    add static export config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next'
 
+const command = process.env.npm_lifecycle_event
+
 const nextConfig: NextConfig = {
+  output: command === 'build' ? 'export' : undefined,
   allowedDevOrigins: ['192.168.1.*'],
   sassOptions: {
     additionalData: `
