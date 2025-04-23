010e1ffde08b465ee5c19034ec07d2915bbed10b
mattfisher <nuggsrocks@yahoo.com>
2025-04-23 00:02:41 -0400

    adjust nextjs config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from 'next'
 const command = process.env.npm_lifecycle_event
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   output: command === 'build' ? 'export' : undefined,
+  trailingSlash: true,
+  images: {
+    loaderFile: './imageLoader.ts',
+    unoptimized: true,
+  },
   allowedDevOrigins: ['192.168.1.*'],
   sassOptions: {
     additionalData: `
