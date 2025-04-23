888056705578a383ed5c10d094d663b3ca4bf8a6
mattfisher <nuggsrocks@yahoo.com>
2025-04-23 00:23:56 -0400

    style contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Script from 'next/script'
 import PageHeader from '../components/PageHeader/PageHeader'
+import Container from '../components/Container/Container'
 
 export default function Contact() {
+  const [loaded, setLoaded] = useState(false)
+
   const injectForm = () => {
     const moxieFrame: HTMLElement | null = document.getElementById(
       'moxie-website-contact-form',
@@ -15,9 +18,13 @@ export default function Contact() {
       moxieFrame.src =
         'https://hello.withmoxie.com/01/crackerjack-solutions/website-contact-form?inFrame=true&sourceUrl=' +
         encodeURIComponent(window.location.href)
+
+      moxieFrame.onload = () => {
+        setLoaded(true)
+      }
       setTimeout(
         () =>
-          /* @ts-expect-error included in cdn script*/
+          /* @ts-expect-error included in cdn script below */
           iFrameResize(
             {
               heightCalculationMethod: 'min',
@@ -56,22 +63,27 @@ export default function Contact() {
     <main>
       <PageHeader title='Contact Us' />
 
-      <Script
-        type='text/javascript'
-        src='https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.10/iframeResizer.min.js'
-      ></Script>
-      <div style={{ width: '100%', minHeight: '500px' }}>
-        <iframe
-          id='moxie-website-contact-form'
-          style={{
-            padding: '0px',
-            margin: '0px',
-            border: '0',
-            maxWidth: '100%',
-            minWidth: '100%',
-          }}
-        ></iframe>
-      </div>
+      <Container>
+        <Script
+          type='text/javascript'
+          src='https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.10/iframeResizer.min.js'
+        ></Script>
+        <div style={{ width: '100%', minHeight: '600px' }}>
+          <iframe
+            id='moxie-website-contact-form'
+            style={{
+              padding: '0px',
+              margin: '0px',
+              border: 'none',
+              maxWidth: '100%',
+              minWidth: '100%',
+              display: loaded ? 'inline' : 'none',
+            }}
+          ></iframe>
+
+          <h2 style={{ display: loaded ? 'none' : 'inline' }}>Loading...</h2>
+        </div>
+      </Container>
     </main>
   )
 }
