d2e8b68e7a2bc04c2a28f29bb77b84a203aa18ae
mattfisher <nuggsrocks@yahoo.com>
2025-05-05 21:54:01 -0400

    modify lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  '*.{ts,tsx,mjs,js}': (files) => {
-    return [
-      `npm run lint -- --file ${files.join(' ')}`,
-      'npm run typecheck',
-      `npx prettier ${files.join(' ')} --write`,
-    ]
-  },
+  '*.{ts,tsx,mjs,js}': (files) => [
+    `npm run lint -- --file ${files.join(' ')}`,
+    'npm run typecheck',
+    `npx prettier ${files.join(' ')} --write`,
+  ],
+  '!(*.ts|*.tsx|*.mjs|*.js)': (files) => [
+    `npx prettier ${files.join(' ')} --write`,
+  ],
 }
