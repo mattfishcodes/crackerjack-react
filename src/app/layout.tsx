f93eb8db6da09f73190ab3c17a35712c5425675c
mattfisher <nuggsrocks@yahoo.com>
2025-06-03 19:03:25 -0400

    remove cookieyes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,6 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import './globals.scss'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
-import Script from 'next/script'
 
 config.autoAddCss = false
 
@@ -50,18 +49,6 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        {process.env.NODE_ENV === 'production' && (
-          <>
-            <Script
-              id='cookieyes'
-              src='https://cdn-cookieyes.com/client_data/ec6aea4d0b987f40283650e6/script.js'
-            ></Script>
-            <Script
-              id='cookieyes-banner'
-              src='https://cdn-cookieyes.com/client_data/ec6aea4d0b987f40283650e6/banner.js'
-            ></Script>
-          </>
-        )}
         <link
           rel='icon'
           type='image/png'
