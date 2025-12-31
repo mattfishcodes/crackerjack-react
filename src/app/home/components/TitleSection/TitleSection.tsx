29c73327ba834a3bc2d8f5d5b8ce967f87e88ebf
mattfisher <nuggsrocks@yahoo.com>
2025-12-30 19:47:43 -0500

    convert TitleSection

--- src/app/home/components/TitleSection/TitleSection.tsx
+++ src/app/home/components/TitleSection/TitleSection.tsx
@@ -1,42 +1,42 @@
 'use client'
 
 import Image from 'next/image'
-import styles from './TitleSection.module.scss'
-import Container from '@/app/components/Container/Container'
 import Separator from '@/components/Separator'
 
 const TitleSection = () => {
   return (
-    <Container bg='primary'>
-      <div className={styles.container}>
-        <div className={styles.item}>
-          <Image
-            src='/images/icon-square.png'
-            alt=''
-            width={800}
-            height={800}
-            sizes='(max-width: 768px) 300px, 800px'
-            priority
-          />
-        </div>
-        <div className={styles.item}>
-          <h1>Crackerjack Solutions</h1>
-          <Separator variant='light' />
-          <p>
-            Welcome to Crackerjack Solutions Virtual Business Management
-            Services!
-          </p>
-          <p>
-            Empowering Entrepreneurs and Business Leaders to cultivate business
-            growth, elevate productivity, and optimize operations.
-          </p>
-          <p>
-            Unleash Your Potential, Reduce Stress, and Reclaim Time for What
-            Matters Most!
-          </p>
+    <section className='bg-primary flex items-center justify-center p-[2rem_1rem] text-white'>
+      <div className='w-full max-w-300'>
+        <div className='flex flex-col items-center justify-between gap-4 text-center lg:flex-row'>
+          <div className='lg:max-w-1/4'>
+            <Image
+              src='/images/icon-square.png'
+              alt=''
+              width={800}
+              height={800}
+              sizes='(max-width: 768px) 300px, 800px'
+              priority
+            />
+          </div>
+          <div>
+            <h1>Crackerjack Solutions</h1>
+            <Separator variant='light' />
+            <p>
+              Welcome to Crackerjack Solutions Virtual Business Management
+              Services!
+            </p>
+            <p>
+              Empowering Entrepreneurs and Business Leaders to cultivate
+              business growth, elevate productivity, and optimize operations.
+            </p>
+            <p>
+              Unleash Your Potential, Reduce Stress, and Reclaim Time for What
+              Matters Most!
+            </p>
+          </div>
         </div>
       </div>
-    </Container>
+    </section>
   )
 }
 
