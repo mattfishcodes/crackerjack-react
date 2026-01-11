789e1f7b3e23093cbb82809f8e8acd086ae1a9ec
mattfisher <nuggsrocks@yahoo.com>
2026-01-11 00:25:49 -0500

    add shadcn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,98 +1,82 @@
 'use client'
 
-import { useRef, useState } from 'react'
-import Image from 'next/image'
+import { useState } from 'react'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import routes from '@/app/routes'
-import { Menu, X } from 'lucide-react'
-import { cn, useLockBodyScroll } from '@/lib/utils'
+import { Menu } from 'lucide-react'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuLink,
+} from './ui/navigation-menu'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from './ui/sheet'
+import { Button } from './ui/button'
 
 export default function Navbar() {
-  const pn = usePathname()
-  const ref = useRef<HTMLElement>(null)
-
   const [open, setOpen] = useState(false)
 
-  useLockBodyScroll(open)
-
   return (
-    <header className='flex flex-col items-center justify-center border-b-2 border-gray-400'>
-      <div className='flex h-16 w-full items-center justify-between px-4 lg:max-w-300 lg:p-0'>
-        <Link href='/' className='h-12 w-12'>
-          <Image
-            src='/icon.svg'
-            alt='Crackerjack Solutions logo'
-            width={180}
-            height={180}
-            priority
-            unoptimized
-          />
-        </Link>
+    <header className='container mx-auto'>
+      <div className='flex w-full items-center justify-between'>
+        <div>Brand</div>
 
-        <nav className='hidden lg:flex lg:gap-2'>
-          {routes.map((r, i) => {
-            return (
-              <Link
-                className={cn(
-                  'hover:bg-primary rounded-full px-3 py-2 transition-colors hover:text-white',
-                  pn === r.href && 'bg-primary text-secondary',
-                )}
-                key={i}
-                href={r.href}
-              >
-                {r.name}
-              </Link>
-            )
-          })}
-        </nav>
+        {/* Desktop Nav */}
+        <NavigationMenu className='hidden md:flex'>
+          <NavigationMenuList>
+            {routes.map((r, i) => {
+              return (
+                <NavigationMenuItem key={i}>
+                  <NavigationMenuLink asChild>
+                    <Link href={r.href}>{r.name}</Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              )
+            })}
+          </NavigationMenuList>
+        </NavigationMenu>
 
-        <button
-          onClick={() => setOpen((v) => !v)}
-          className='text-primary cursor-pointer p-1 lg:hidden'
-        >
-          <Menu strokeWidth={3} />
-        </button>
+        {/* Mobile Nav */}
+        <Sheet open={open} onOpenChange={setOpen}>
+          <SheetTrigger asChild className='md:hidden'>
+            <Button variant='outline' size='icon'>
+              <span className='sr-only'>Open menu</span>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+
+          {/* OFFCANVAS */}
+          <SheetContent side='right' className='w-75 text-center sm:w-100'>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+            </SheetHeader>
+
+            <div className='flex flex-col items-center'>
+              <NavigationMenu>
+                <NavigationMenuList>
+                  {routes.map((r, i) => {
+                    return (
+                      <NavigationMenuItem key={i}>
+                        <NavigationMenuLink asChild>
+                          <Link href={r.href} onClick={() => setOpen(false)}>
+                            {r.name}
+                          </Link>
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                    )
+                  })}
+                </NavigationMenuList>
+              </NavigationMenu>
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
-
-      {open && (
-        <div
-          onClick={() => setOpen(false)}
-          className='fixed inset-0 z-30 bg-black/40'
-        />
-      )}
-
-      <nav
-        ref={ref}
-        className={cn(
-          'fixed inset-x-0 top-0 z-40 bg-white',
-          'origin-top transform-gpu',
-          'transition-all duration-300 ease-out',
-          'pointer-events-none -translate-y-2 scale-y-95 opacity-0',
-          'flex flex-col',
-          'motion-reduce:transition-none',
-          'text-right',
-          open && 'pointer-events-auto translate-y-0 scale-y-100 opacity-100',
-        )}
-      >
-        <div className='px-4 py-2'>
-          <button onClick={() => setOpen(false)}>
-            <X size={36} />
-          </button>
-        </div>
-        {routes.map((r, i) => {
-          return (
-            <Link
-              className='active:text-secondary active:bg-primary px-4 py-2 text-xl'
-              key={i}
-              href={r.href}
-              onClick={() => setOpen((v) => !v)}
-            >
-              {r.name}
-            </Link>
-          )
-        })}
-      </nav>
     </header>
   )
 }
