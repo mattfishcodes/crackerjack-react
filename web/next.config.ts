19827cf614f89172b42843f80a3896b41e5e94a9
Matthew Fisher <mattfishcodes@gmail.com>
2026-04-20 19:25:14 -0400

    chore: set next config trailing slash to true

--- web/next.config.ts
+++ web/next.config.ts
@@ -8,6 +8,7 @@ const nextConfig: NextConfig = {
 
   // important for siteground
   output: command === 'build' ? 'export' : undefined,
+  trailingSlash: true,
 
   images: {
     loaderFile: './imageLoader.ts',
