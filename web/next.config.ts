bc915b73da3c31a7d76107a01939778389b9677c
Matthew Fisher <mattfishcodes@gmail.com>
2026-04-22 00:27:35 -0400

    build: migrate deployment target from siteground to vercel

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,17 +1,10 @@
 import type { NextConfig } from 'next'
 
-const command = process.env.npm_lifecycle_event
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: ['192.168.1.*'],
 
-  // important for siteground
-  output: command === 'build' ? 'export' : undefined,
-  trailingSlash: true,
-
   images: {
-    loaderFile: './imageLoader.ts',
     deviceSizes: [576, 768, 992, 1200, 1400],
     imageSizes: [16, 32, 256],
   },
